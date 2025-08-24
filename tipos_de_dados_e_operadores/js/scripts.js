266151ff988257ab808d0dd68279446cd7caf182
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-24 13:59:18 -0300

    operações aritméticas

--- tipos_de_dados_e_operadores/js/scripts.js
+++ tipos_de_dados_e_operadores/js/scripts.js
@@ -1,5 +1,12 @@
 // Number
 
-console.log(typeof 2); // typeof = tipo do dado
-console.log(typeof 5.14);
-console.log(typeof -127);
+console.log(typeof 2) // typeof = tipo do dado
+console.log(typeof 5.14)
+console.log(typeof -127)
+
+// Operações aritméticas
+
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(10 / 2)
+console.log(10 * 2)
