25e9b9851ff814d62c848aa92c34bad522153e91
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-24 16:11:02 -0300

    mais sobre variáveis

--- estruturas_de_programacao/js/scripts.js
+++ estruturas_de_programacao/js/scripts.js
@@ -8,3 +8,10 @@ console.log(nome)
 
 const idade = 22; // constante - não muda de valor
 console.log(idade)
+
+let a = 10, b = 20, c = 30 // criando várias variáveis
+console.log(a, b, c)
+
+let _teste = "ok" // com _ é permitido
+let $teste = "ok" // com $ é permitido
+console.log(_teste,$teste)
