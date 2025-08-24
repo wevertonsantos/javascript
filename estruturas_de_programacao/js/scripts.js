18c6a754f00d84af579a7e16cb778beaa50c5067
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-24 16:06:03 -0300

    variáveis

---
+++ estruturas_de_programacao/js/scripts.js
@@ -0,0 +1,10 @@
+// Variáveis
+
+let nome = "Weverton" // palavra let para declaração
+console.log(nome)
+
+nome = "Weverton Santos" // alteração de valor
+console.log(nome)
+
+const idade = 22; // constante - não muda de valor
+console.log(idade)
