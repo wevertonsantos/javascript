12d0fa07afe4df80dc21797bdc420e686c1b94ad
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-25 14:01:51 -0300

    escopo da função

--- funcoes/js/scripts.js
+++ funcoes/js/scripts.js
@@ -33,4 +33,18 @@ const soma = function (n1, n2) {
 
 const resultado = soma(a, b)
 console.log(resultado)
-console.log(soma(c,d))
+console.log(soma(c, d))
+
+// Escopo da função
+let y = 10
+
+function testandoEscopo() {
+    let y = 20
+    console.log(`Y dentro da função é: ${y}`)
+}
+
+testandoEscopo()
+
+console.log(`Y fora da função é: ${y}`)
+
+testandoEscopo()
