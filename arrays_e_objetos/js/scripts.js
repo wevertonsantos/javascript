23e2dd4efd1d485caf38d9a7bf90773d3654e6db
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-25 14:53:41 -0300

    propriedades e acessando elementos do array

--- arrays_e_objetos/js/scripts.js
+++ arrays_e_objetos/js/scripts.js
@@ -6,3 +6,15 @@ console.log(typeof lista)
 
 const itens = ["Weverton",true,2,4.12,[]]
 console.log(itens)
+
+const arr = ["a", "b", "c", "d"]
+console.log(arr[0]) // acessando primeiro elemento por índice
+console.log(arr[2])
+
+// propriedades
+const numbers = [5, 3, 4]
+console.log(numbers.length) // quantidade de elementos
+console.log(numbers['length'])
+
+const myName = "Weverton"
+console.log(myName.length)
