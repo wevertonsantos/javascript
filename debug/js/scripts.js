df6cb54a04a3cb30096bb4caa3eca817fbd727e6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-26 15:43:48 -0300

    debugger

--- debug/js/scripts.js
+++ debug/js/scripts.js
@@ -27,3 +27,25 @@ if (a > 5) {
 }
 
 console.log(a)
+
+// debugger
+let c = 1
+let d = 2
+
+if (c === 1) {
+    c = d + 2
+}
+
+debugger
+
+for (let i = 0; i < d; i ++) {
+    c = c + 2
+}
+
+console.log("executou o loop")
+
+debugger
+
+if (c > 5) {
+    c = 25
+}
