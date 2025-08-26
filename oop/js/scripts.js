70821a3ec7c97274f9cfe2cb7e10f31692980d21
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-26 14:32:47 -0300

    classes básicas

--- oop/js/scripts.js
+++ oop/js/scripts.js
@@ -37,3 +37,18 @@ console.log(Object.getPrototypeOf(myObject))
 const mySecondObject = Object.create(myObject)
 console.log(mySecondObject)
 console.log(mySecondObject.a)
+
+// Classes básicas
+const cachorro = {
+    raca: null,
+    patas: 4
+}
+
+const pastorAlemao = Object.create(cachorro)
+pastorAlemao.raca = "Pastor Alemão"
+console.log(pastorAlemao)
+console.log(pastorAlemao.patas)
+
+const bulldog = Object.create(cachorro)
+bulldog.raca = "Bulldog"
+console.log(bulldog)
