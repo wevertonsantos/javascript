f08c0a6dcba96fe6966986328c368168433b23fa
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-26 15:54:22 -0300

    utilizando try catch

--- debug/js/scripts.js
+++ debug/js/scripts.js
@@ -72,5 +72,12 @@ checkNumber("Teste")
 let x = 10
 
 if (x !== 11) {
-    throw new Error("O valor de x não pode ser diferente de 11!")
+    // throw new Error("O valor de x não pode ser diferente de 11!")
+}
+
+// try catch
+try {
+    const soma = x + y
+} catch (error) {
+    console.log(`Erro no programa: ${error}`)
 }
