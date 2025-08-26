bf09e6b5d435d7828baa03e00410ebb54f6aa77a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-26 15:57:22 -0300

    finally

--- debug/js/scripts.js
+++ debug/js/scripts.js
@@ -81,3 +81,15 @@ try {
 } catch (error) {
     console.log(`Erro no programa: ${error}`)
 }
+
+// finally
+try {
+    const value = checkNumber("asd")
+    if (!value) {
+        throw new Error("Valores inválidos")
+    }
+} catch (error) {
+    console.log(`Opa, aconteceu um problema: ${error}`)
+} finally {
+    console.log("O código foi executado!")
+}
