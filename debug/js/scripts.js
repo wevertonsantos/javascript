3c671b57f0966275e24542468ddf3755f4128342
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-26 15:37:55 -0300

    depurando com console.log

--- debug/js/scripts.js
+++ debug/js/scripts.js
@@ -7,3 +7,23 @@ console.log(opa)
 
 // const undefined = 10
 // delete [].length
+
+// console.log
+let a = 1
+let b = 2
+if (a === 1) {
+    a = b + 2
+}
+
+console.log(a)
+
+for (let i = 0; i < b; i++) {
+    a = a + 2
+    console.log(a)
+}
+
+if (a > 5) {
+    a = 25
+}
+
+console.log(a)
