edcbeb41a3d9c1809491c88a59602a580ae9d563
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 15:21:27 -0300

    começando com DOM

---
+++ DOM/js/scripts.js
@@ -0,0 +1,4 @@
+// Movendo-se pelo DOM
+console.log(document.body)
+console.log(document.body.childNodes[1].childNodes) // nós
+console.log(document.body.childNodes[1].childNodes[1].textContent) // acessando texto 
