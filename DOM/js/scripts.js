e292f16e0f2fefb60e3203e49fda3ba1b8424f00
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 15:26:54 -0300

    selecionando por tag

--- DOM/js/scripts.js
+++ DOM/js/scripts.js
@@ -1,4 +1,8 @@
 // Movendo-se pelo DOM
 console.log(document.body)
 console.log(document.body.childNodes[1].childNodes) // nós
-console.log(document.body.childNodes[1].childNodes[1].textContent) // acessando texto 
+console.log(document.body.childNodes[1].childNodes[1].textContent) // acessando texto
+
+// Selecionando por TAG
+const listItens = document.getElementsByTagName("li")
+console.log(listItens)
