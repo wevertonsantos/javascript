d45379e5242978b31e86b0c2a03107e49f789c49
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 14:23:10 -0300

    promise e falha na promise

--- async/js/scripts.js
+++ async/js/scripts.js
@@ -16,3 +16,22 @@ setInterval(function () {
 },3000)
 
 console.log("Ainda não começou")
+
+// Promise - é uma promessa de um valor que pode chegar em um ponto futuro
+
+const promessa = Promise.resolve(5 + 5)
+console.log("Algum código")
+
+promessa.then(value => {
+    console.log(`A soma é: ${value}`)
+}) // utilizando o valor da promessa com then
+
+console.log("Outro código")
+
+// Falha na promise
+Promise.resolve(4 * "asd")
+    .then((n) => {
+        if (Number.isNaN(n)) {
+            throw new Error("Valores inválidos")
+        }
+    }).catch((err) => console.log(`Um erro ocorreu: ${err}`))
