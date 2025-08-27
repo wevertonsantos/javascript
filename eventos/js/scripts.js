c6e15dc93ee5fdab0d56dccea6ecef496707ff4c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 16:50:03 -0300

    removendo evento

--- eventos/js/scripts.js
+++ eventos/js/scripts.js
@@ -5,3 +5,17 @@ const btn = $.querySelector("#my-button")
 btn.addEventListener("click", () => {
     console.log("Clicou aqui!")
 })
+
+// - Removendo evento
+const secondBtn = $.querySelector("#btn")
+const imprimirMensagem = () => {
+    console.log("Teste")
+}
+secondBtn.addEventListener("click", imprimirMensagem)
+
+const thirdBtn = $.querySelector("#other-btn")
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido")
+    // removendo evento com função
+    secondBtn.removeEventListener("click",imprimirMensagem)
+})
