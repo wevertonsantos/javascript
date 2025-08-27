9d414af10497f1db478e9872337ad3ce192c9ec7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 14:31:42 -0300

    rejeição promise

--- async/js/scripts.js
+++ async/js/scripts.js
@@ -35,3 +35,19 @@ Promise.resolve(4 * "asd")
             throw new Error("Valores inválidos")
         }
     }).catch((err) => console.log(`Um erro ocorreu: ${err}`))
+
+// Rejeição promise
+function checkNumber(n) {
+    return new Promise((resolve, reject) => {
+        if (n > 10) {
+            resolve(`O número é maior que 10`)
+        } else {
+            reject(new Error("Número muito baixo"))
+        }
+    })
+}
+
+const a = checkNumber(20)
+const b = checkNumber(10)
+
+a.then((v) => console.log(`O resultado é: ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`))
