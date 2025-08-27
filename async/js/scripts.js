f1c3ba1734a5a346245267a6bc6630764f4a7808
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-27 14:10:26 -0300

    setTimeout

---
+++ async/js/scripts.js
@@ -0,0 +1,9 @@
+// setTimeout
+console.log("Ainda não executou")
+
+// executada depois de um certo tempo
+setTimeout(function () {
+    console.log("Requisição assíncrona")
+}, 2000) // passando função e milisegundos
+
+console.log("Ainda não executou 2")
