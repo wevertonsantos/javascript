6fc2bc978f02eec50ea84af487f0368be2efbc9d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-28 16:42:46 -0300

    evento keyup e keydown

--- eventos/js/scripts.js
+++ eventos/js/scripts.js
@@ -52,3 +52,12 @@ a.addEventListener("click", (e) => {
     e.preventDefault()
     console.log("Não alterou a página")
 })
+
+// - Eventos de tecla
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`)
+})
