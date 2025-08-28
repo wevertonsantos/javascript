94a434b3172418ae3c67ca1ed5486b08158ac8a5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-28 17:06:17 -0300

    debounce

--- eventos/js/scripts.js
+++ eventos/js/scripts.js
@@ -108,3 +108,23 @@ window.addEventListener("beforeunload", (e) => {
     e.preventDefault()
     e.returnValue = ""
 })
+
+// - Debounce
+const debounce = (f, delay) => {
+    let timeout
+
+    return (...arguments) => {
+        if (timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        },delay)
+    }
+}
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Executando a cada 400ms")
+}, 400)
+)
