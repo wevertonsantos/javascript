33930c35e1cf0c9930c3df438e1ab3bb7c0fad33
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-09-10 13:59:46 -0300

    spread operator

--- JS_moderno/js/scripts.js
+++ JS_moderno/js/scripts.js
@@ -117,3 +117,19 @@ const productDetails = {
 
 const { name: productName, price, category, color } = productDetails
 console.log(`Nome do produto é ${productName}, ele custa R$${price} e é da cor ${color}`)
+
+// spread operator
+const a1 = [1,2,3]
+const a2 = [4, 5, 6]
+const a3 = [...a1, ...a2]
+console.log(a3)
+
+const a4 = [0, ...a3, 7]
+console.log(a4)
+
+const carName = { name: "Gol" }
+const carBrand = { brand: "VW" }
+const otherInfos = { km: 100000, price: 49999 }
+
+const car = { ...carName, ...carBrand, ...otherInfos }
+console.log(car)
